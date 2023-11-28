a1aec64ddfb3b3ae50f1b75269edf5161b4f0f18
Angel Hernandez <angel.hdz.ojd@gmail.com>
2023-11-28 16:45:34 +0000

    Last commit, with the test.

--- src/kata2/Main.java
+++ src/kata2/Main.java
@@ -7,5 +7,8 @@ public class Main {
     public static void main(String[] args) {
         List<Person> people = new TsvFileLoader(new File("persons.tsv")).load();
         Map<String, Integer> hist = new WeightHistogram(20, people).calculate();
+        for(String key : hist.keySet().stream().sorted().toList()){
+            System.out.println(key + " " + hist.get(key));
+        }
     }
 }
