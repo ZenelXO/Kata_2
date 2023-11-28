12c1ee9949355c6f9e9d1e21140564be76624285
Angel Hernandez <angel.hdz.ojd@gmail.com>
2023-11-28 16:44:09 +0000

    Second commit, with all the necessaries functions and class, only missing the test.

--- src/kata2/Main.java
+++ src/kata2/Main.java
@@ -1,4 +1,11 @@
 package kata2;
 
+import java.io.File;
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new TsvFileLoader(new File("persons.tsv")).load();
+        Map<String, Integer> hist = new WeightHistogram(20, people).calculate();
+    }
 }
